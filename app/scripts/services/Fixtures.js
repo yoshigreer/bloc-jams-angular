a941698f3d2a8772b286db18544ae72b5fb2f7f5
Trenton Greer <yoshigreer@gmail.com>
2016-12-01 22:38:56 -0700

    assignment 6 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
       return albumPicasso;
     };
 
+    Fixtures.getCollection = function(numberOfAlbums) {
+      var albums = [];
+      for (var i = 0; i < numberOfAlbums; i++) {
+        albums.push(this.getAlbum());
+      }
+      return albums;
+    };
+
     return Fixtures;
   }
 
