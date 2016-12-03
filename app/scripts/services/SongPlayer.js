cddb5ea815007220d2b51d8d101e7d41cccda82b
Trenton Greer <yoshigreer@gmail.com>
2016-12-03 14:20:51 -0700

    assignment done, but pause button remains when i click next or back

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -21,8 +21,7 @@
     */
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(song);
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -41,6 +40,16 @@
     var playSong = function(song) {
       currentBuzzObject.play();
       song.playing = true;
+    };
+
+    /**
+    * @function stopSong
+    * @desc
+    * @param {Object} song
+    */
+    var stopSong = function(song) {
+      currentBuzzObject.stop();
+      song.playing = null;
     };
 
     /**
@@ -95,8 +104,7 @@
       currentSongIndex++;
 
       if (currentSongIndex == currentAlbum.songs.length) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(SongPlayer.currentSong);
       } else {
         var song = currentAlbum.songs[currentSongIndex];
         setSong(song);
@@ -113,8 +121,7 @@
       currentSongIndex--;
 
       if (currentSongIndex < 0) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(SongPlayer.currentSong);
       } else {
         var song = currentAlbum.songs[currentSongIndex];
         setSong(song);
